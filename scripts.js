8e920a31992c9b6ec1dc2776b6e4c856be4e649e
roxioxx <roxioxx@gmail.com>
2022-01-07 16:54:41 -0500

    Basic Upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+import * as Realm from "realm-web";
+
+const app = new Realm.App({ id: "cookbook-famra" });
+
+// Create an anonymous credential
+const credentials = Realm.Credentials.anonymous();
+try {
+    // Authenticate the user
+    const user = await app.logIn(credentials);
+    // `App.currentUser` updates to match the logged in user
+    assert(user.id === app.currentUser.id)
+
+    return user
+} catch (err) {
+    console.error("Failed to log in", err);
+}
+
+console.log(user);
