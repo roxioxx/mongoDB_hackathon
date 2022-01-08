ede6fcb492e10f4ac61828fa935b60c44951e338
roxioxx <roxioxx@gmail.com>
2022-01-08 15:53:58 -0500

    Added Formatting

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ async function connectToDB() {
 
 
 async function getDBData() {
-  const mongodb = app.currentUser.mongoClient("mongodb-atlas");
-const recipes = mongodb.db("cookbook").collection("recipes");
+    const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+    const recipes = mongodb.db("cookbook").collection("recipes");
     try {
         const venusFlytrap = await recipes.find();
         console.log("all recipes", venusFlytrap);
@@ -24,7 +24,63 @@ const recipes = mongodb.db("cookbook").collection("recipes");
     }
 };
 
+
+
+function formatCards(array) {
+    const arr = array.forEach((element) => {
+        //Just Creating the Card for Each
+        var node = document.createElement("div"); // Create a <div> node
+
+
+        var nodeInner = ` <div class="col flip-container" ontouchstart="this.classList.toggle('hover');">
+        <div class="flipper">
+
+            <div class="front">
+                <!-- front content -->
+
+                <div class="card">
+                    <img src="images/${element.shortname}.png">
+                    <div class="card-container">
+                        <h2>${element._id}</h2>
+                        <p>${element.tagLine}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="back">
+                <!-- back content -->
+
+                <div class="card">
+                    <div class="card-container">
+                        <h2>${element._id}</h2>
+                        <a href="${element.link}" class="btn" target="_blank">${element.linkText}</a>
+
+                        <br>
+                        <h3>INGREDIENTS</h3>
+                        <ul> 
+                        <li>${element.writtenIngredients.join("</li><li>")}
+                        </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+`;
+
+        node.innerHTML = nodeInner;
+        //Finally, Appending that card to the index.html
+        document.getElementById("recipesHere").appendChild(node);
+
+        //Sanity Check
+        console.log(element._id);
+    })
+};
+
+const nodel = document.getElementById("recipesHere");
+
 window.addEventListener('load', (event) => {
     connectToDB();
-    getDBData();
+    formatCards(getDBData());
 });
