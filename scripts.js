5b4020b5e5bb604261a115be6167e5794de29baa
roxioxx <roxioxx@gmail.com>
2022-01-07 21:39:41 -0500

    added no sniff

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-import * as Realm from "/realm.js"
-
 const app = new Realm.App({ id: "cookbook-famra" });
 const mongodb = app.currentUser.mongoClient("mongodb-atlas");
 const recipes = mongodb.db("cookbook").collection("recipes");
@@ -7,16 +5,19 @@ const recipes = mongodb.db("cookbook").collection("recipes");
 
 // Create an anonymous credential
 const credentials = Realm.Credentials.anonymous();
-try {
-    // Authenticate the user
-    const user = await app.logIn(credentials);
-    // `App.currentUser` updates to match the logged in user
-    assert(user.id === app.currentUser.id);
-    console.log(user);
 
-} catch (err) {
-    console.error("Failed to log in", err);
+function connectToDB(credentials, err) {
+    try {
+        // Authenticate the user
+        const user = await app.logIn(credentials);
+        // `App.currentUser` updates to match the logged in user
+        assert(user.id === app.currentUser.id);
+        console.log(user);
+
+    } catch (err) {
+        console.error("Failed to log in", err);
+    }
+    const venusFlytrap = await recipes.find();
+    console.log("all recipes", venusFlytrap);
+
 }
-
-const venusFlytrap = await recipes.find();
-console.log("all recipes", venusFlytrap);
